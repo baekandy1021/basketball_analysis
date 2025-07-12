ac23c3593dcd1ce8ba8ea294a268a516d62a3936
unknown <baek.andy1021@gmail.com>
2025-07-12 01:35:53 -0500

    player team assignment

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from drawers import(
     PlayerTracksDrawer,
     BallTracksDrawer
 )
+from team_assigner import TeamAssigner
 def main():
     #read video
     video_frames = read_video("input_videos/video_1.mp4")
@@ -13,13 +14,18 @@ def main():
     ball_tracker = BallTracker("models/ball_detector_model.pt")
 
     #Run trackers
-    player_tracks = player_tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/player_track_stubs.pk1")
-    ball_tracks = ball_tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/ball_track_stubs.pk1")
+    player_tracks = player_tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/player_track_stubs.pkl")
+    ball_tracks = ball_tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/ball_track_stubs.pkl")
 
     #Remove wrong ball detections
     ball_tracks = ball_tracker.remove_wrong_detections(ball_tracks)
     #Interpolate ball tracks
     ball_tracks=ball_tracker.interpolate_ball_positions(ball_tracks)
+
+    #Assign Player Teams
+    team_assigner = TeamAssigner()
+    player_assignment = team_assigner.get_player_teams_accross_frames(video_frames, player_tracks, read_from_stubs=True, stub_path="stubs/player_assignment.pkl")
+    
 
     #Draw Output
     #Initialize Drawers
@@ -27,7 +33,7 @@ def main():
     ball_tracks_drawer =BallTracksDrawer()
 
     # Draw Object Tracks
-    output_video_frames=player_tracks_drawer.draw(video_frames,player_tracks)
+    output_video_frames=player_tracks_drawer.draw(video_frames,player_tracks, player_assignment)
     output_video_frames=ball_tracks_drawer.draw(output_video_frames, ball_tracks)
 
     #save video
